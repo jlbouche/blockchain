2514bad677d1b5e4440e5d8280056bb8a0b2480e
JL <jennalbouche@gmail.com>
2021-06-15 11:26:58 -0700

    setup newblock prototype to blockchain

---
+++ dev/blockchain.js
@@ -0,0 +1,24 @@
+function Blockchain() {
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        //nonce is proof that newBlock was created legitimately using proof of work method
+        nonce: nonce,
+        //hash is the data from the new/current block, hashed into a string
+        hash: hash,
+        //previousBlockHash is data from previous block, hashed into a string
+        previousBlockHash: previousBlockHash
+    }
+    //clearing newTransactions
+    this.newTransactions = [];
+    //adding new block to blockchain array
+    this.chain.push(newBlock);
+
+    return newBlock;
+}
