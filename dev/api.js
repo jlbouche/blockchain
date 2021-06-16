e387c99e015316c3f2970e36e9c45ca59e54e0a7
JL <jennalbouche@gmail.com>
2021-06-16 11:56:13 -0700

    install bodyparser, add transaction post functionality in api.js, test in postman

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,29 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello Blockchain dev!')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//get full blockchain
+app.get('/blockchain', function (req, res) {
+    res.send(bitcoin);
 })
- 
-app.listen(3000)
+
+//post new transaction
+app.post('/transaction', function (req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({ note: `Transaction will be added in block ${blockIndex}.`})
+})
+
+//create new block
+app.get('/mine', function(req, res){
+
+})
+
+app.listen(3000, function(){
+    console.log('Listening on port 3000...');
+})
