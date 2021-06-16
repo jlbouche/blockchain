c1a8471ce75e0bf69b805a71d086984e80a65920
JL <jennalbouche@gmail.com>
2021-06-16 15:48:50 -0700

    install uuid dependencies, add functionality for /mine in api.js

--- dev/api.js
+++ dev/api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const uuid = require('uuid').v4;
+
+const nodeAddress = uuid().split('-').join('');
 
 const bitcoin = new Blockchain();
 
@@ -21,7 +24,37 @@ app.post('/transaction', function (req, res) {
 
 //create new block
 app.get('/mine', function(req, res){
+    //because createNewBlock has 3 params, have to define these params below
+    //and each function that requires params defined below
 
+    //need previous block to get new block
+    const lastBlock = bitcoin.getLastBlock();
+
+    //get hash from previous block using hash property
+    const previousBlockHash = lastBlock['hash'];
+
+    //current block data needed due to proof of work params needed for nonce and hashblock below
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    }
+    //get nonce using proof of work
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+
+    //create hash for new block using params in hashBlock function, defined above
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    //reward mine creator by giving new transaction for bitcoin, sender is "00" to
+    //reflect it is a mining reward, recipient is this network node created using uuid
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+    //all params are now available to run createNewBlock
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    })
 })
 
 app.listen(3000, function(){
